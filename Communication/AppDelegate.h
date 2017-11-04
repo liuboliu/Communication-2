3d8658a448a5b4ad2c1ed5459a07ce8a16f9ceca
岳岳淼 <FS.Jay@wifi.local>
2017-11-04 10:47:06 +0800

    Initial Commit

---
+++ Communication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Communication
+//
+//  Created by Legendary.Bie on 2017/11/4.
+//  Copyright © 2017年 Smile_bie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
